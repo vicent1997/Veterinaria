20681fc268c34ed3cb3a0a0684d4f948fbec6851
christianfus <christianfus@gmail.com>
2019-12-12 19:08:52 +0100

    yepale

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.scss"]
+  styleUrls: ["./home.component.scss"],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
   constructor() {}
@@ -11,18 +12,17 @@ export class HomeComponent implements OnInit {
   images: any[];
 
   ngOnInit() {
-    this.images = [];
-    this.images.push({
-      source: "assets/images/Catadau/Catadau1.jpeg",
-      alt: "Description for Image 1",
-      title: "Title 1"
-      
-    });
-    this.images.push({
-      source: "assets/images/Catadau/Catadau2.jpeg",
-      alt: "Description for Image 2",
-      title: "Title 2",
-      
-    });
+    this.images = [
+      {
+        source: "assets/images/Catadau/Catadau1.jpeg",
+        alt: "Description for Image 1",
+        title: "Title 1"
+      },
+      {
+        source: "assets/images/Catadau/Catadau2.jpeg",
+        alt: "Description for Image 2",
+        title: "Title 2"
+      }
+    ];
   }
 }
