a678dc58762f64ada5a493f9d7b29af56121cbe0
christianfus <christianfus@gmail.com>
2019-12-14 23:48:02 +0100

    SICK CHANGES BITCH

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-about-us',
-  templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.scss']
+  selector: "app-about-us",
+  templateUrl: "./about-us.component.html",
+  styleUrls: ["./about-us.component.scss"]
 })
 export class AboutUsComponent implements OnInit {
-
-  constructor() { }
+  options: any;
+  images: any[];
+  constructor() {}
 
   ngOnInit() {
-  }
+    this.options = {
+      center: { lat: 36.890257, lng: 30.707417 },
+      zoom: 12
+    };
 
+    this.images = [
+      {
+        source: "assets/images/perritos/perrito1.jpg",
+        alt: "Description for Image 1",
+        title: "Title 1"
+      },
+      {
+        source: "assets/images/perritos/perrito3.jpg",
+        alt: "Description for Image 2",
+        title: "Title 2"
+      }
+    ];
+  }
 }
