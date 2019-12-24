d4fddda306fd95fcf215d1f192f06e22348e32dc
vicent1997 <rosanatividadvicent@gmail.com>
2019-12-24 16:36:07 +0100

    push cuz shit happens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,8 @@ export class AppComponent {
   public mapLlosa: any;
   public images: any[];
   public descripcion: string;
-  public horarios: string;
+  public horario: string;
+  public horarioSabado: string;
   public titulo: any;
   public overlays: any;
 
@@ -48,35 +49,35 @@ export class AppComponent {
       this.titulo = "Catadau";
       this.images = [
         "assets/images/Catadau/fachada.jpeg",
-        "assets/images/perritos/perrito2.jpg",
-        "assets/images/perritos/doctor-ahogando-perro.jpg"
+        "assets/images/Catadau/instalaciones1.jpeg",
+        "assets/images/Catadau/instalaciones2.jpeg"
       ];
       this.descripcion = `Esta clínica veterinaria está abierta desde el año 1998. 
-      Más de 30 años de experiencia como veterinario.
       Colaborador de la asociación SOS Animalades.
       En nuestra clínica veterinaria disponemos de servicios de diagnóstico, consultas y medicina general, cirugía, análisis clínicos, vacunaciones y medicina preventiva.
       También disponemos de una sección de tienda especializada con un amplia gama de productos para el cuidado y la alimentación de los perros y gatos. 
       Se ofrece servicio de atención domiciliaria. Trabajamos con gran entusiasmo y dedicación para cuidar de tu mejor amigo.`;
 
-      this.horarios = `Catadau Lunes - Viernes: 18:00 - 21:00
-      Sabados 17:00 - 19:00`;
+      this.horario = '18:00 - 21:00';
+      this.horarioSabado = '17:00 - 19:00';
     }
     if (clinica === "llosa") {
       this.titulo = "Llosa";
       this.images = [
-        "assets/images/perritos/doctor-ahogando-perro.jpg",
-        "assets/images/perritos/doctor-ahogando-perro.jpg",
-        "assets/images/perritos/doctor-ahogando-perro.jpg"
+        "assets/images/Llosa/fachada.jpeg",
+        "assets/images/Llosa/instalaciones1.jpeg",
+        "assets/images/Llosa/instalaciones2.jpeg"
       ];
       this.descripcion = `Esta clínica veterinaria está abierta desde el año 1999. 
-      Más de 30 años de experiencia como veterinario.
       Colaborador de la asociación SOS Animalades.
       En nuestra clínica veterinaria disponemos de servicios de diagnóstico, consultas y medicina general, cirugía, análisis clínicos, vacunaciones y medicina preventiva.
       También disponemos de una sección de tienda especializada con un amplia gama de productos para el cuidado y la alimentación de los perros y gatos. 
       Se ofrece servicio de atención domiciliaria. Trabajamos con gran entusiasmo y dedicación para cuidar de tu mejor amigo.`;
-      this.horarios = `La Llosa lunes-Viernes: 10:30 -13:00
+      this.horario = `10:30 -13:00
+      
       17:00 -20:00
-      Sábados: 11;00 - 13:00`;
+      `;
+      this.horarioSabado = "11;00 - 13:00";
     }
   }
 
